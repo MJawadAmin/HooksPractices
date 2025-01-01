a022647eb7cf4a3dfad356677e99491b3cb72ba1
Jaddikhan <114812094+Jaddikhan@users.noreply.github.com>
2025-01-01 17:35:58 +0500

    useEffect

--- Hooks/src/Components/Hooks.jsx
+++ Hooks/src/Components/Hooks.jsx
@@ -1,13 +1,19 @@
-import {useState} from 'react'
+import {useCallback, useEffect, useState} from 'react'
 
 const Hooks = () => {
     const [length , setLength] = useState(8);
     const [numberAllowed, setNumberAllowed] = useState(false);
     const [charAllowed, setCharAllowed] = useState(false);
-    const [password, setPassword] = useState("");
-    const PasswordGanerator=()=>{
-        if (numberAllowed) str+= "01234567689"
+    const [password, setPassword] = useState(""); 
+    const PasswordGenarator= useEffect(()=>{
+        const pass= ""
+        const str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+        for (let i =   1; i < length; i++) {
+            let char = Math.floor(Math.random)* str.length + 1
+
     }
+        
+},[length , numberAllowed, charAllowed, setPassword])
   return (
     <div>
       
